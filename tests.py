ba8958c940b76e1ad62df5b324a8ff39121c8f33
Chandra Kiran Evuru <cs15btech11012@iith.ac.in>
2019-05-09 19:17:22 +0530

    Updated .travis.yml and added tests.py

---
+++ tests.py
@@ -0,0 +1,9 @@
+import os
+import qdarkgraystyle
+
+from shutil import copyfile
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon, QPixmap, QImage
+from PyQt5.QtCore import QObject, pyqtSlot, QThread, pyqtSignal, QSize
+from PyQt5.QtWidgets import QLabel, QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, QDialog
